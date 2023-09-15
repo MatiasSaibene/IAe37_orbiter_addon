202b009abbad4ee7777b51cdf068cf220c7d9f09
MatiasSaibene <matiassaibene98@gmail.com>
2023-09-15 02:25:03 -0300

    First commit

---
+++ Mesh/include.cpp
@@ -0,0 +1,74 @@
+// Auto generated code file.  Blender: 3.3.9  Blender Tools: (2, 1, 2)
+// Date: Fri Sep 15 01:27:44 2023
+
+
+#include "orbitersdk.h"
+
+#ifndef __IA37_H
+#define __IA37_H
+
+namespace bl 
+{
+
+// Scene IA37
+
+  namespace IA37 
+  {
+    const DWORD TXIDX_Wheel_dds = 1;
+    const DWORD TXIDX_Fuselage_dds = 2;
+    const DWORD TXIDX_Rudder_dds = 3;
+    constexpr auto MESH_NAME = "IA37";
+
+    const UINT CockpitId = 0;
+    const UINT EnginesId = 1;
+    const UINT Front_landing_gearId = 2;
+    const UINT Front_landing_gear_wheelId = 3;
+    const UINT FuselageId = 4;
+    const UINT Rear_landing_gears_wheelsId = 5;
+    const UINT Rear_left_landing_gearId = 6;
+    const UINT Rear_right_landing_gearId = 7;
+    const UINT RudderId = 8;
+    const UINT TailId = 9;
+    const UINT WindowsId = 10;
+    const UINT hlaileronId = 11;
+    const UINT hraileronId = 12;
+    constexpr VECTOR3 Eng0_posLocation =     {-0.7349, 0.0060, -5.7835};
+    constexpr VECTOR3 Eng1_posLocation =     {0.7351, 0.0060, -5.7835};
+    constexpr VECTOR3 ElevatorLocation =     {0.0016, 0.0053, -5.3407};
+    constexpr VECTOR3 Cockpit_posLocation =     {0.0011, 0.0052, 2.6594};
+    constexpr VECTOR3 FuselageLocation =     {0.0016, 0.0040, -1.4850};
+    constexpr VECTOR3 CockpitLocation =     {0.0016, 0.0052, 3.4530};
+    constexpr VECTOR3 hlaileronLocation =     {-3.9179, 0.0054, -5.3924};
+    constexpr VECTOR3 EnginesLocation =     {0.0016, 0.0052, -4.8541};
+    constexpr VECTOR3 Front_landing_gear_wheelLocation =     {0.0005, -1.5630, 1.6607};
+    constexpr VECTOR3 Front_landing_gearLocation =     {0.0005, -1.0475, 1.8598};
+    constexpr VECTOR3 Rear_right_landing_gearLocation =     {1.0492, -0.8505, -1.5848};
+    constexpr VECTOR3 Rear_landing_gears_wheelsLocation =     {0.0022, -1.3308, -1.5248};
+    constexpr VECTOR3 Rear_left_landing_gearLocation =     {-1.0473, -0.8505, -1.5848};
+    constexpr VECTOR3 TailLocation =     {-0.0103, 0.7903, -3.5322};
+    constexpr VECTOR3 RudderLocation =     {-0.0104, 0.9376, -5.6396};
+    constexpr VECTOR3 hraileronLocation =     {3.9211, 0.0054, -5.3924};
+    constexpr VECTOR3 WindowsLocation =     {0.0016, 0.0052, 3.4530};
+    constexpr VECTOR3 TdP_GU1Location =     {0.0028, -0.5800, 0.2794};
+    constexpr VECTOR3 TdP_GU1.001Location =     {0.0028, -0.2800, -6.0706};
+    constexpr VECTOR3 TdP_GU1.002Location =     {-4.9247, -0.0098, -5.6777};
+    constexpr VECTOR3 TdP_GU1.003Location =     {-0.0103, 1.8772, -5.6777};
+    constexpr VECTOR3 TdP_GU1.004Location =     {0.0009, 0.5900, 0.1679};
+    constexpr VECTOR3 TdP_GU1.005Location =     {0.0009, -0.0001, 4.2095};
+    constexpr VECTOR3 TdP_GU1.006Location =     {4.8753, -0.0098, -5.6777};
+    constexpr VECTOR3 TdP_GDLocation =     {-0.0004, -1.7648, 1.6610};
+    constexpr VECTOR3 TdP_GD.001Location =     {-1.0480, -1.5333, -1.5250};
+    constexpr VECTOR3 TdP_GD.002Location =     {1.0535, -1.5333, -1.5250};
+    constexpr VECTOR3 TdP_GD.003Location =     {-4.8480, -1.5333, -5.6311};
+    constexpr VECTOR3 TdP_GD.004Location =     {4.8535, -1.5333, -5.6311};
+    constexpr VECTOR3 Axis_front_landing_gearLocation =     {0.0024, -0.4404, 2.0604};
+    constexpr VECTOR3 Axis_rear_landing_gearLocation =     {0.0024, -0.4404, -1.7396};
+    constexpr VECTOR3 Axis_hraileronLocation =     {3.6649, 0.0062, -4.9072};
+    constexpr VECTOR3 Axis_hlaileronLocation =     {-3.6393, 0.0062, -4.9072};
+    constexpr VECTOR3 Axis_rudderLocation =     {-0.0108, 1.0781, -5.4007};
+    constexpr VECTOR3 Axis_elevatorLocation =     {-0.0108, 0.0062, -4.9072};
+
+  }
+
+}
+#endif
---
+++ Mesh/include_cockpit.cpp
@@ -0,0 +1,26 @@
+// Auto generated code file.  Blender: 3.3.9  Blender Tools: (2, 1, 2)
+// Date: Fri Sep 15 01:47:16 2023
+
+
+#include "orbitersdk.h"
+
+#ifndef __IA37_cockpit_H
+#define __IA37_cockpit_H
+
+namespace bl 
+{
+
+// Scene IA37_cockpit
+
+  namespace IA37 
+  {
+    constexpr auto MESH_NAME = "IA37_cockpit";
+
+    const UINT CockpitId = 0;
+    constexpr VECTOR3 CockpitLocation =     {0.0000, 0.0000, 0.0000};
+    constexpr VECTOR3 CameraLocation =     {0.0016, 0.0153, -0.1937};
+
+  }
+
+}
+#endif
